56784b49d5989ea0bb5b0b281ea8f74a6d97c8b2
4ist <frichardson062@gmail.com>
2020-09-11 18:32:35 -0400

    added home component outline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DevBlogsComponent } from './components/dev-blogs/dev-blogs.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   //{ path: 'DevBlogs', component: DevBlogsComponent }
-  { path: '', component: DevBlogsComponent },
+  { path: '', component: HomeComponent },
+  { path: 'DevBlogs', component: DevBlogsComponent },
   { path: 'Projects', component: ProjectsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DevBlogsComponent,
     HeaderComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
